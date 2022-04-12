b356da2fb0f2504d5caddc2f5adb3fe6d73d764d
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2022-04-12 15:04:32 -0700

    Mushroom
    Setup Mushroom prefab to be destructible

--- projectCode/Centipede/Assets/Scripts/Mushroom.cs
+++ projectCode/Centipede/Assets/Scripts/Mushroom.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Sprite[] states;
+
+    private int health;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        health = states.Length;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Damage(int amount)
     {
-        
+        health -= amount;
+
+        if (health > 0)
+        {
+            sr.sprite = states[states.Length - health]; // Encapsulate into separate method later
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Dart"))
+        {
+            Damage(1);
+        }
     }
 }
